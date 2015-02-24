391440a70a0cc6578d96ebda3983f6bc58dea2b2
Jordan Stewart <nj.jstewart@gmail.com>
2015-02-24 16:59:34 -0600

    Still trying to ajax client data by unique id

--- public/dashboard/dashboard_quickstart/ajax/getClientList.php
+++ public/dashboard/dashboard_quickstart/ajax/getClientList.php
@@ -8,23 +8,32 @@ left outer join dbo.Company on dbo.Company.Company_RecID = dbo.Company_Address.C
 left outer join dbo.Company_Type on dbo.Company_Type.Company_Type_RecID = dbo.Company.Company_Type_RecID
 left outer join dbo.Company_Status on dbo.Company_Status.Company_Status_RecID = dbo.Company.Company_Status_RecID
 WHERE (dbo.Company_Status.Description = "Active" and 
-dbo.Company_Type.Description = "Client" and dbo.Company_Address.Default_Flag=1) OR (dbo.Company_Status.Description = "Active - Security" and dbo.Company_Type.Description = "Client" AND dbo.Company_Address.Default_Flag=1 )');
+dbo.Company_Type.Description = "Client" and dbo.Company_Address.Default_Flag=1) OR (dbo.Company_Status.Description = "Active - Security" and dbo.Company_Type.Description = "Client" AND dbo.Company_Address.Default_Flag=1 ) order by dbo.company.Company_Name');
 
-echo "<div style='padding:0px;margin-left:10px; margin-top:10px;'class=' col-md-3 panel panel-default'>";
+echo "<div style='width:100%;padding:0px;margin-left:10px; margin-top:10px;'class=' panel panel-default'>";
 echo "<div class='panel-heading'>Clients</div>";
-echo "<div style='overflow-y: scroll !important;height:500px'class=panel-body>";
+echo "<div style='width:100%;overflow-y: scroll !important;height:500px'class=panel-body>";
 echo "<table id='clientTable' style='width:100%;' class='table table-hover'>";
 echo "<thead>";
 echo "<th></th>";
 echo "</thead>";
 
-echo "<tbody data-link='row' class='rowlink'>";
+echo "<tbody  class='rowlink'>";
 // fetch all rows from the query
 //$all_rows = array();
+$id=0;
 while($row = mssql_fetch_array($clientList)) {
+
+	$str = $row['Company_Name'];
+
+	if (strlen($str) > 30){
+		$str = substr($str, 0, 25) . '...';
+	}
+   
 	echo "<tr>";
-    echo "<td><a id='colink'href='#clientData?clientData=".$row['Company_Name']."'>".$row['Company_Name']."</a></td>";
+    echo "<td><input type='submit' id='colink".$id."'href='?company=".$row['Company_Name']."'value='".$str."'/></td>";
     echo "</tr>";
+    $id++;
 }
 echo "</tbody>";
 echo "</table>";
